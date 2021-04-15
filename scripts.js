c0a4cf9779a719eb51eff239f50335bfe3c08575
Livia Brasil <liviabrasil@id.uff.br>
2021-04-15 20:51:56 -0300

    entrada na sala

--- scripts.js
+++ scripts.js
@@ -1,1 +1,20 @@
-const request = axios.post(`https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants](https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants)`)
+let username = prompt ("Qual é o seu nome?");
+
+function verifyName () {
+const requestname = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/participants",{name: username});
+requestname.then(setInterval(presenceControll, 5000))
+requestname.catch(repeatPrompt)
+}
+
+verifyName ();
+
+function presenceControll (dataname) {
+    axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/status", {name: username});
+}
+
+function repeatPrompt () {
+    username = prompt("Digite um nome que não esteja em uso");
+    verifyName ();
+}
+
+
