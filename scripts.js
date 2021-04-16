aeede86a0099fd27344bab65e352891dd30edb1e
Livia Brasil <liviabrasil@id.uff.br>
2021-04-15 22:42:13 -0300

    mensagens separadas por cor

--- scripts.js
+++ scripts.js
@@ -17,4 +17,56 @@ function repeatPrompt () {
     verifyName ();
 }
 
+function getData () {
+    const info = axios.get("https://mock-api.bootcamp.respondeai.com.br/api/v2/uol/messages");
+    info.then(renderMessages);
+}
+
+getData();
+
+function renderMessages (content) {
+
+    const chat = document.querySelector(".chat");
+    let messages;
+
+    console.log (content);
+    for(i=0 ; i<content.data.length ; i++) {
+
+        if (content.data[i].type === "status"){
+            messageContent =
+            `<div class="message gray">
+                <p class=time>${content.data[i].time}</p>
+                <p><span class=name ${content.data[i].type}>${content.data[i].from}</span> para <span class=name>${content.data[i].to}</span>: ${content.data[i].text}</p>
+            </div>
+            `
+        }
+        if (content.data[i].type === "private_message"){
+            messageContent =
+            `<div class="message red">
+                <p class=time>${content.data[i].time}</p>
+                <p><span class=name ${content.data[i].type}>${content.data[i].from}</span> para <span class=name>${content.data[i].to}</span>: ${content.data[i].text}</p>
+            </div>
+            `
+        }
+        if (content.data[i].type === "message"){
+            messageContent =
+            `<div class="message white">
+                <p class=time>${content.data[i].time}</p>
+                <p><span class=name ${content.data[i].type}>${content.data[i].from}</span> para <span class=name>${content.data[i].to}</span>: ${content.data[i].text}</p>
+            </div>
+            `
+        }
+
+
+        messages += messageContent;
+        chat.innerHTML = messages;
+    } 
+        
+
+   
+
+
+}
+
+setInterval (getData,3000)
 
